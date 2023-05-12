e9154867fb200a1004675fe3e4adbab8c72295ee
kunnalbharrgav7 <101549156+kunnalbharrgav7@users.noreply.github.com>
2023-05-12 19:00:41 +0530

    Add files via upload

---
+++ quick sort.py
@@ -0,0 +1,12 @@
+#. Implement a quick sort algorithm for an array of integers
+
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    
+    return quick_sort(left) + middle + quick_sort(right)
