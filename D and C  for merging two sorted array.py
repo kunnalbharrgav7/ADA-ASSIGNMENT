06acb56094b5fdf25131debcde3f681289bdae2b
kunnalbharrgav7 <101549156+kunnalbharrgav7@users.noreply.github.com>
2023-05-12 19:44:49 +0530

    Add files via upload

---
+++ D and C  for merging two sorted array.py
@@ -0,0 +1,41 @@
+#Implement a divide-and-conquer algorithm for merging two sorted arrays.
+
+def merge(arr1, arr2):
+    if len(arr1) == 0:
+        return arr2
+    elif len(arr2) == 0:
+        return arr1
+    
+    result = []
+    i = j = 0
+    while i < len(arr1) and j < len(arr2):
+        if arr1[i] < arr2[j]:
+            result.append(arr1[i])
+            i += 1
+        else:
+            result.append(arr2[j])
+            j += 1
+    
+    while i < len(arr1):
+        result.append(arr1[i])
+        i += 1
+    
+    while j < len(arr2):
+        result.append(arr2[j])
+        j += 1
+    
+    return result
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    
+    mid = len(arr) // 2
+    left = arr[:mid]
+    right = arr[mid:]
+    
+    left = merge_sort(left)
+    right = merge_sort(right)
+    
+    return merge(left, right)
+
