fa874f8179b5322176994defb615c4f8abb7d47c
kunnalbharrgav7 <101549156+kunnalbharrgav7@users.noreply.github.com>
2023-05-13 21:53:43 +0530

    Add files via upload

---
+++ LCS_DP_NP Completeness.py
@@ -0,0 +1,13 @@
+# Implement a dynamic programming algorithm for the Longest Common Subsequence Problem (LCS).
+
+def lcs_length(X, Y):
+    m = len(X)
+    n = len(Y)
+    c = [[0] * (n+1) for i in range(m+1)]
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+            if X[i-1] == Y[j-1]:
+                c[i][j] = c[i-1][j-1] + 1
+            else:
+                c[i][j] = max(c[i-1][j], c[i][j-1])
+    return c[m][n]
